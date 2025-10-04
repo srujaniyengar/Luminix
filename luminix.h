afaa4274490afd2df91da650fcb5457b01e90f4f
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-10-04 12:33:07 +0530

    add: luminix.h

---
+++ luminix.h
@@ -0,0 +1,6 @@
+#ifndef LUMINIX_H
+#define LUMINIX_H
+
+float luminance(unsigned char r, unsigned char g, unsigned char b);
+int luminix(const char *src_path, const char *tgt_path, const char *out_path);
+#endif // LUMINIX_H
